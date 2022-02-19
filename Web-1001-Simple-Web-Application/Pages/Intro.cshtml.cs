aaca383e567f01af070a922030126695bbb8d28d
savithamk <80167929+savithamk@users.noreply.github.com>
2022-02-19 13:33:24 -0500

    Added new Razor page
    Added new Razor page to the project named as Intro

---
+++ Web-1001-Simple-Web-Application/Pages/Intro.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_1001_Simple_Web_Application.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
